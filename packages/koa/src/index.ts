5eddd5fdd01c0333b69357b25846fb1ea76073e9
s-r-x <strax1990@gmail.com>
2021-04-26 06:47:06 +0500

    koa. use router prefix instead absolute path. add middleware init param. call next after rendering the ui

--- packages/koa/src/index.ts
+++ packages/koa/src/index.ts
@@ -1,21 +1,31 @@
 import { BullMonitor } from '@bull-monitor/root';
 import { ApolloServer } from 'apollo-server-koa';
 import Router from 'koa-router';
+import type { Middleware } from 'koa';
 
+export type InitParams = {
+  middleware?: Middleware;
+};
 export class BullMonitorKoa extends BullMonitor<ApolloServer> {
   public router: Router;
-  async init() {
-    const router = new Router();
+  public async init({ middleware }: InitParams = {}) {
+    const router = new Router({
+      prefix: this.baseUrl,
+    });
     this.createServer(ApolloServer);
     await this.server.start();
-    const middleware = this.server.getMiddleware({
+    const apolloMiddleware = this.server.getMiddleware({
       path: this.gqlEndpoint,
     });
-    router.get(this.gqlEndpoint, middleware);
-    router.post(this.gqlEndpoint, middleware);
-    router.get(this.uiEndpoint, ctx => {
+    if (middleware) {
+      router.use(middleware);
+    }
+    router.get(this.gqlBasePath, apolloMiddleware);
+    router.post(this.gqlBasePath, apolloMiddleware);
+    router.get('/', async (ctx, next) => {
       ctx.type = 'text/html';
       ctx.body = this.renderUi();
+      await next();
     });
     this.router = router;
   }
