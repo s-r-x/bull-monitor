0ae8fd5c13109cf1da228cd73b33decff6cee2cb
s-r-x <strax1990@gmail.com>
2021-04-26 05:02:07 +0500

    move apollo-server-core to devDependencies inside root / import gql from apollo-server-core instead of graphql-tag

--- packages/root/src/gql/type-defs/job.ts
+++ packages/root/src/gql/type-defs/job.ts
@@ -1,5 +1,4 @@
-import gql from 'graphql-tag';
-
+import { gql } from 'apollo-server-core';
 
 export const jobTypeDef = gql`
   enum JobStatus {
--- packages/root/src/gql/type-defs/redis-info.ts
+++ packages/root/src/gql/type-defs/redis-info.ts
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import { gql } from 'apollo-server-core';
 
 
 export const redisInfoTypeDef = gql`
