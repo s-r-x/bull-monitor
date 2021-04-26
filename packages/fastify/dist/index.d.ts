0c514fa7c113a9eca92d7e7179bad61ff5d41c5d
s-r-x <strax1990@gmail.com>
2021-04-26 05:04:44 +0500

    add fastify/dist to gitignore

--- packages/fastify/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { BullMonitor } from '@bull-monitor/root';
-import { ApolloServer } from 'apollo-server-fastify';
-import type { FastifyPluginCallback } from 'fastify';
-export declare class BullMonitorFastify extends BullMonitor<ApolloServer> {
-    plugin: FastifyPluginCallback;
-    init(): Promise<void>;
-}
