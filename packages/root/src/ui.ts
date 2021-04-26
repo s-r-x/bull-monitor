8f6861e5f6b362f4121cb699adcbd0d437daff94
s-r-x <strax1990@gmail.com>
2021-04-26 08:59:14 +0500

    bump root/ui packages minor ver

--- packages/root/src/ui.ts
+++ packages/root/src/ui.ts
@@ -1,5 +1,5 @@
 export class UI {
-  private uiVersion = '0.1.1';
+  private uiVersion = '0.2.0';
   private pkgName = '@bull-monitor/ui';
   private cdnRoot = `https://cdn.jsdelivr.net/npm/${this.pkgName}@${this.uiVersion}`;
   private resourcesRoot = this.cdnRoot + '/build/dist';
