640678c6a55dcec751f7dab82715e5e4a067a996
s-r-x <strax1990@gmail.com>
2021-04-27 08:37:07 +0500

    fix clean queue demo mock

--- packages/ui/src/demoMocks/network/mutations/clean-queue.ts
+++ packages/ui/src/demoMocks/network/mutations/clean-queue.ts
@@ -9,11 +9,14 @@ import { networkMockData } from '../data';
 export const cleanQueueMock = (
   args: CleanQueueMutationVariables,
 ): Promise<CleanQueueMutation> => {
-  networkMockData.jobs = networkMockData.jobs.filter(
-    (job) =>
-      job.queue !== args.queue &&
+  networkMockData.jobs = networkMockData.jobs.filter((job) => {
+    if (job.queue !== args.queue) {
+      return true;
+    }
+    return (
       job.status !==
-        (args.status === JobStatusClean.Wait ? JobStatus.Waiting : args.status),
-  );
+      (args.status === JobStatusClean.Wait ? JobStatus.Waiting : args.status)
+    );
+  });
   return Promise.resolve({ cleanQueue: [] });
 };
