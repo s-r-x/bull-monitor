a863c1520c62ca921de138ff4392a03d984a77ed
Wei <weilinzung@gmail.com>
2021-05-11 15:53:09 -0400

    fix pagination overlay issue

--- packages/ui/src/screens/jobs/List/Pagination/index.tsx
+++ packages/ui/src/screens/jobs/List/Pagination/index.tsx
@@ -3,15 +3,30 @@ import TablePagination from '@material-ui/core/TablePagination';
 import { usePaginationStore } from '@/stores/pagination';
 import { useCount } from './hooks';
 import { PaginationConfig } from '@/config/pagination';
+import { useThemeStore } from '@/stores/theme';
 
 export default function Pagination() {
   const { page, perPage, changePage, changePerPage } = usePaginationStore();
+  const theme = useThemeStore((state) => state.theme);
   const count = useCount();
+  const stickyPagination = {
+    dark: {
+      bg: '#424242',
+      border: '#515151',
+    },
+    light: {
+      bg: '#fff',
+      border: '#e0e0e0',
+    },
+  };
+
   return (
     <TablePagination
       style={{
         position: 'sticky',
         bottom: 0,
+        borderTop: `1px solid ${stickyPagination[theme].border}`,
+        backgroundColor: stickyPagination[theme].bg,
       }}
       rowsPerPageOptions={PaginationConfig.perPageOptions}
       component="div"
