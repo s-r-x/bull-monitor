8418d621e97a6a7302fda72b177e1c18399381ad
s-r-x <strax1990@gmail.com>
2021-05-17 14:35:56 +0500

    chore(ui): useToggle hooks

---
+++ packages/ui/src/hooks/use-toggle.ts
@@ -0,0 +1,9 @@
+import { useState, useCallback } from 'react';
+
+export const useToggle = (defaultState = false): [boolean, () => void] => {
+  const [value, setValue] = useState(defaultState);
+  const toggle = useCallback(() => {
+    setValue((v) => !v);
+  }, []);
+  return [value, toggle];
+};
