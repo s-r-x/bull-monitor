d5b38091bb40aef19d2ea17aca2e44c6c7c1c5ef
s-r-x <strax1990@gmail.com>
2021-05-17 20:45:47 +0500

    perf(ui): remove calling persisted stores in the root component. zustand restores them without it

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Shell from '@/components/Shell';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { getMuiTheme, useThemeStore } from '@/stores/theme';
+import { getMuiTheme } from '@/stores/theme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import JobsScreen from '@/screens/jobs';
 import { ConfirmProvider } from 'material-ui-confirm';
 import { SnackbarProvider } from 'notistack';
 import { NetworkProvider } from './providers/network';
 import { useRunStoreSideEffects } from './stores/side-effects';
-import { usePreferencesStore } from './stores/preferences';
-import { useNetworkSettingsStore } from './stores/network-settings';
-import { useQueuesFilterStore } from './stores/queues-filter';
 import { QueuesQueryProvider } from './providers/queues-query';
 
 const queryClient = new QueryClient();
@@ -18,10 +15,6 @@ const queryClient = new QueryClient();
 export default function App() {
   const muiTheme = getMuiTheme();
   useRunStoreSideEffects();
-  usePreferencesStore();
-  useQueuesFilterStore();
-  useNetworkSettingsStore();
-  useThemeStore();
   return (
     <NetworkProvider>
       <MuiThemeProvider theme={muiTheme}>
