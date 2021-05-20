aacab19b23bf8c3139468be2e00490c53dab3855
s-r-x <strax1990@gmail.com>
2021-05-20 10:12:18 +0500

    rebuild demo

--- docs/dist/hooks/use-toggle.js
+++
@@ -1,8 +0,0 @@
-import {useState, useCallback} from "../../_snowpack/pkg/react.js";
-export const useToggle = (defaultState = false) => {
-  const [value, setValue] = useState(defaultState);
-  const toggle = useCallback(() => {
-    setValue((v) => !v);
-  }, []);
-  return [value, toggle, setValue];
-};
