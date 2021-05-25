e08708a9ce8dae8ef9b9155f56cf6ea86a9e76ac
s-r-x <strax1990@gmail.com>
2021-05-25 11:00:09 +0500

    fix(ui): reset page on data search key/term change

--- packages/ui/src/stores/pagination.ts
+++ packages/ui/src/stores/pagination.ts
@@ -26,7 +26,13 @@ export const useRunPaginationSideEffects = () => {
     const effect = () => changePage(0);
     const unsubFilters = useFiltersStore.subscribe(
       effect,
-      (state) => [state.order, state.status, state.jobId],
+      (state) => [
+        state.order,
+        state.status,
+        state.jobId,
+        state.changeDataSearchKey,
+        state.changeDataSearchTerm,
+      ],
       shallow,
     );
     const unsubActiveQueue = useActiveQueueStore.subscribe(
