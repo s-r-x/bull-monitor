ddaadcb9fbe032f1bec66a8d6514554e31f71453
s-r-x <strax1990@gmail.com>
2021-06-07 12:25:14 +0500

    feat(ui): persist per page option

--- packages/ui/src/stores/pagination.ts
+++ packages/ui/src/stores/pagination.ts
@@ -2,8 +2,10 @@ import createStore from 'zustand';
 import { useActiveQueueStore } from './active-queue';
 import shallow from 'zustand/shallow';
 import { useEffect } from 'react';
+import { persist } from 'zustand/middleware';
 import { useFiltersStore } from './filters';
 import { PaginationConfig } from '@/config/pagination';
+import { StorageConfig } from '@/config/storage';
 
 type TState = {
   perPage: number;
@@ -12,13 +14,21 @@ type TState = {
   changePage: (page: number) => void;
 };
 
-export const usePaginationStore = createStore<TState>((set) => ({
-  page: 0,
-  perPage: PaginationConfig.perPageOptions[1],
+export const usePaginationStore = createStore<TState>(
+  persist(
+    (set) => ({
+      page: 0,
+      perPage: PaginationConfig.perPageOptions[1],
 
-  changePage: (page) => set({ page }),
-  changePerPage: (perPage) => set({ perPage }),
-}));
+      changePage: (page) => set({ page }),
+      changePerPage: (perPage) => set({ perPage }),
+    }),
+    {
+      whitelist: ['perPage'],
+      name: `${StorageConfig.persistNs}pagination`,
+    },
+  ),
+);
 
 export const useRunPaginationSideEffects = () => {
   const changePage = usePaginationStore((state) => state.changePage);
