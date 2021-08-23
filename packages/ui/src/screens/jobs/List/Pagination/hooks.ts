9c102adc1d5c76427e6ac41b1911df2676b9e646
s-r-x <strax1990@gmail.com>
2021-08-23 08:47:45 +0500

    fix(ui): pages count is always zero if search by job id is not empty

--- packages/ui/src/screens/jobs/List/Pagination/hooks.ts
+++ packages/ui/src/screens/jobs/List/Pagination/hooks.ts
@@ -1,16 +1,14 @@
 import { useQueueData } from '@/hooks/use-queue-data';
-import { activeQueueAtom, activeStatusAtom } from '@/atoms/workspaces';
+import { activeQueueAtom, activeStatusAtom, jobIdAtom } from '@/atoms/workspaces';
 import { useAtomValue } from 'jotai/utils';
 import { JobStatus } from '@/typings/gql';
 
 export const useCount = (): number => {
+  const jobId = useAtomValue(jobIdAtom);
   const status = useAtomValue(activeStatusAtom);
   const activeQueue = useAtomValue(activeQueueAtom) as string;
   const jobsCounts = useQueueData(activeQueue)?.jobsCounts;
-  if (!jobsCounts) {
-    return 0;
-  }
-  if (status === JobStatus.Stuck) {
+  if (!jobsCounts || jobId || status === JobStatus.Stuck) {
     return 0;
   }
   if (status in jobsCounts) {
