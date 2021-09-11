5e1f0649e995af39e762aeebd15dd048d5ca4870
s-r-x <strax1990@gmail.com>
2021-09-11 13:24:39 +0500

    feat(ui): more distinguishable colors for statuses

--- packages/ui/src/components/JobStatusChip/hooks.ts
+++ packages/ui/src/components/JobStatusChip/hooks.ts
@@ -1,41 +1,21 @@
 import { JobStatus } from '@/typings/gql';
-import { useTheme } from '@material-ui/core/styles';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 import grey from '@material-ui/core/colors/grey';
 import cyan from '@material-ui/core/colors/cyan';
-import { useMemo } from 'react';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/blue';
 
-export const useJobStatusesPalette = () => {
-  const theme = useTheme();
-  return useMemo(() => {
-    const mapping: Record<JobStatus, string> = {
-      [JobStatus.Failed]: theme.palette.error.main,
-      [JobStatus.Completed]: theme.palette.success.main,
-      [JobStatus.Delayed]: theme.palette.info.main,
-      [JobStatus.Waiting]: deepPurple[500],
-      [JobStatus.Paused]: grey[600],
-      [JobStatus.Active]: cyan[700],
-      [JobStatus.Stuck]: grey[400],
-    };
-    return mapping;
-  }, [theme]);
+const palette: Record<JobStatus, string> = {
+  [JobStatus.Failed]: red[500],
+  [JobStatus.Completed]: green[500],
+  [JobStatus.Delayed]: blue[800],
+  [JobStatus.Waiting]: deepPurple[500],
+  [JobStatus.Paused]: grey[600],
+  [JobStatus.Active]: cyan[500],
+  [JobStatus.Stuck]: grey[400],
 };
+export const useJobStatusesPalette = () => palette;
 export const useJobStatusColor = (status: JobStatus): string => {
-  const theme = useTheme();
-  switch (status) {
-    case JobStatus.Failed:
-      return theme.palette.error.main;
-    case JobStatus.Completed:
-      return theme.palette.success.main;
-    case JobStatus.Delayed:
-      return theme.palette.info.main;
-    case JobStatus.Waiting:
-      return deepPurple[500];
-    case JobStatus.Paused:
-      return grey[600];
-    case JobStatus.Active:
-      return cyan[700];
-    default:
-      return theme.palette.primary.main;
-  }
+  return palette[status];
 };
