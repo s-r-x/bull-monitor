30d9b205c3d3abf76222dba1c687996d69436dc0
s-r-x <strax1990@gmail.com>
2021-09-24 13:10:33 +0500

    chore(ui): remove meta.hot

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -10,8 +10,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
