5f4dc5710bd50148f632d5e1546cee08a6012321
s-r-x <strax1990@gmail.com>
2022-01-10 17:49:02 +0500

    chore: bullmq fixture

--- fixtures/bull-server/server/index.ts
+++ fixtures/bull-server/server/index.ts
@@ -1,13 +1,24 @@
 import { BullMonitorExpress } from '@bull-monitor/express';
 import { BullAdapter } from '@bull-monitor/root/bull-adapter';
+import { BullMQAdapter } from '@bull-monitor/root/bullmq-adapter';
 import express from 'express';
 import Queue from 'bull';
+import {
+  Queue as BullMqQueue,
+  Worker as MqWorker,
+  QueueScheduler as MqQueueScheduler,
+  ConnectionOptions,
+} from 'bullmq';
 
 const QUEUES_AMOUNT = 5;
 const READONLY_QUEUES_AMOUNT = 2;
 const redisUri = process.env.REDIS_URI as string;
+const redisHost = 'redis';
 const port = process.env.PORT;
 const app = express();
+const mqConnOptions: ConnectionOptions = {
+  host: redisHost,
+};
 
 const queues = Array.from(new Array(QUEUES_AMOUNT)).map(
   (_v, idx) => new Queue(`queue:${idx}`, redisUri)
@@ -31,9 +42,29 @@ const prefixedQueues = Array.from(new Array(QUEUES_AMOUNT)).map(
     );
   });
 });
+const mqQueues = [
+  new BullMqQueue('mq-1', {
+    connection: mqConnOptions,
+  }),
+];
+new MqQueueScheduler(mqQueues[0].name, {
+  connection: mqConnOptions,
+});
+mqQueues.forEach((queue) => {
+  new MqWorker(
+    queue.name,
+    async (job) => {
+      return `some return value from ${job.name}`;
+    },
+    {
+      connection: mqConnOptions,
+    }
+  );
+});
 
 const monitor = new BullMonitorExpress({
   queues: [
+    ...mqQueues.map((queue) => new BullMQAdapter(queue)),
     ...queues.map((queue) => new BullAdapter(queue as any)),
     ...prefixedQueues.map((queue) => new BullAdapter(queue as any)),
     ...readonlyQueues.map(
