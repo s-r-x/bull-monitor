82f9111adc531cb5851099e5316eb62e897286ea
s-r-x <strax1990@gmail.com>
2022-01-11 13:28:49 +0500

    chore: add adapter in nest example

--- examples/nest/src/bull-monitor/bull-monitor.service.ts
+++ examples/nest/src/bull-monitor/bull-monitor.service.ts
@@ -2,10 +2,11 @@ import { BullMonitorExpress } from '@bull-monitor/express';
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { Queue } from 'bull';
+import { BullAdapter } from '@bull-monitor/root/dist/bull-adapter';
 
 @Injectable()
 export class BullMonitorService extends BullMonitorExpress {
   constructor(@InjectQueue('person') personQueue: Queue) {
-    super({ queues: [personQueue] });
+    super({ queues: [new BullAdapter(personQueue)] });
   }
 }
